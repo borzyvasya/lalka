c5d08a50d18eef7dbb9eff1b94371a6ae7eed604
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-01-30 05:26:15 +0300

    finally got it

--- sol1/teaching/dumb.cpp
+++ sol1/teaching/dumb.cpp
@@ -1,35 +1,64 @@
-//#include "std_lib_facilities.h"
-//
-//
-//int main() 
-//{
-//	int n1 = 0, n2 = 0;
-//	cout << "please input first number: "; cin >> n1;
-//	cout << "please input second number: "; cin >> n2;
-//
-//	int difference = 0;
-//	int sum = n1 + n2;
-//	int product = n1 * n2;
-//	int ratio = n1/n2;
-//
-//	if (n1 > n2) { 
-//		difference = n1 - n2; 
-//		cout << "first number > second number\n";
-//	}
-//	else if (n1 < n2) {
-//		difference = n2 - n1;
-//		cout << "first number < second number\n";
-//	}
-//	else {
-//		ratio = 1;
-//		cout << "both numbers are equal \n";
-//	}
-//
-//
-//	cout << "sum: " << sum << "\n"
-//		<< "product: " << product << "\n"
-//		<< "ratio: " << ratio << "\n";
-//
-//
-//	return 0;
-//}
+#include "std_lib_facilities.h"
+
+
+void calc(double value1, char action, double value2) {
+	switch (action) {
+		case '+':
+			cout << value1 << " + " << value2 << " = " << value1 + value2 << "\n";
+			break;
+
+		case '-':
+			cout << value1 << " - " << value2 << " = " << value1 - value2 << "\n";
+			break;
+
+		case '*':
+			cout << value1 << " * " << value2 << " = " << value1 * value2 << "\n";
+			break;
+
+		case '/':
+			cout << value1 << " / " << value2 << " = " << value1 / value2 << "\n";
+			break;
+
+		default:
+			cout << "wrong action..." << "\n";
+	}
+
+}
+
+
+
+int main() 
+{
+	vector<string> values { "zero", "one", "two", "three", "four", 
+							"five", "six", "seven", "eight", "nine" };
+
+
+	string v1, v2;
+	char action;
+	while (cin >> v1 >> action >> v2) {
+		if ((v1.size() == 1 && isdigit(v1[0])) &&
+			(v2.size() == 1 && isdigit(v2[0]))) {
+			int digit1 = v1[0] - '0';
+			int digit2 = v2[0] - '0';
+
+			if ((digit1 >= 0 && digit1 <= 9) &&
+				(digit2 >= 0 && digit2 <= 9)) {
+				calc(digit1, action, digit2);
+			}
+			else cout << "invalid digit\n";
+		}
+
+		else {
+			for (int i = 0; i < values.size(); ++i) {
+				for (int j = 0; j < values.size(); ++j) {
+					if (v1 == values[i] && v2 == values[j]) {
+						calc(i, action, j);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
+}
--- sol1/teaching/testdouble.cpp
+++ sol1/teaching/testdouble.cpp
@@ -1,18 +1,34 @@
-#include "std_lib_facilities.h"
-
-
-
-int main() {
-	vector<string> disliked {"Broccoli", "Bread", "Soup", "Candies", "Pork"};
-
-
-	for (string temp; cin >> temp;) {
-		for (int i = 0; i < disliked.size(); i++) {
-			if (disliked[i] == temp)
-				cout << "BLEEP\n";
-		}
-	}
-	
-
-	return 0;
-}
+//#include "std_lib_facilities.h"
+//
+//
+//
+//int main() {
+//	double v1 = 0.0, v2 = 0.0;
+//	char action = 0;
+//
+//	while (cin >> v1 >> action >> v2) {
+//		switch (action) {
+//		case '+':
+//			cout << v1 << " + " << v2 << " = " << v1 + v2 << "\n";
+//			break;
+//
+//		case '-':
+//			cout << v1 << " - " << v2 << " = " << v1 - v2 << "\n";
+//			break;
+//
+//		case '*':
+//			cout << v1 << " * " << v2 << " = " << v1 * v2 << "\n";
+//			break;
+//
+//		case '/':
+//			cout << v1 << " / " << v2 << " = " << v1 / v2 << "\n";
+//			break;
+//
+//		default:
+//			cout << "wrong action..." << "\n";
+//		}
+//
+//	}
+//
+//	return 0;
+//}
